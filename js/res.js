1096adc0ee19e53a4ccc66dda61de0188c62dcc1
{eunnnnnnn} <pje9978@gmail.com>
2023-10-04 17:31:36 +0900

    1004

--- js/res.js
+++ js/res.js
@@ -1,13 +1,4 @@
 
-// function getData(obj){
-//     const sectionWork = document.querySelector('#work1');
-//     console.log(sectionWork)
-//     const el_H1 = document.createElement('h1');
-//     el_H1.textContent = obj["work"][0].title;
-// }
-
-// populate();
-
 async function populate() {
 
   const requestURL = 'https://github.com/pje9978/portfolio-parallax/blob/main/data/data.json';
@@ -15,13 +6,39 @@ async function populate() {
   const response = await fetch(request);
   const superHeroesText = await response.text();
   const data = JSON.parse(superHeroesText);
-  sectionData(data);
+
+  const rawData = data.payload.blob.rawLines
+  const jsonData = rawData.join('\n');
+  const parsedData = JSON.parse(jsonData);
+
+  const work1 = parsedData.work1[0];
+  console.log(work1)
+  work1Data(work1);
 
 
 }
 populate()
 
-function sectionData(data){
-  const data_Work1 = data.payload.blob.rawLines;
-  console.log(data_Work1)
+function work1Data(work) {
+  if (Array.isArray(work)) {
+    for (const workItem of work) {
+      console.log(workItem);
+      const item = document.createElement('div');
+      const h2Element = document.createElement('h2');
+      const para1Element = document.createElement('p');
+      const para2Element = document.createElement('p');
+      const para3Element = document.createElement('span');
+
+      h2Element.textContent = workItem.title;
+
+      item.appendChild(h2Element);
+      item.appendChild(para1Element);
+      item.appendChild(para2Element);
+      item.appendChild(para3Element);
+
+     // Add the 'item' to the desired location in your HTML document
+    }
+  } else {
+    console.error("Invalid 'work' parameter. Expected an array.");
+  }
 }
