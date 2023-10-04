d07beef0fb662dbab7968c446c20fa11d633ae52
{eunnnnnnn} <pje9978@gmail.com>
2023-10-04 22:25:20 +0900

    1004

--- js/res3.js
+++ js/res3.js
@@ -1,76 +1,104 @@
-var sectionGroup = document.querySelector('#work1');
 
 // 1
 fetch(
     `https://raw.githubusercontent.com/pje9978/portfolio-parallax/main/data/data.json`
-).then(function(response) {
-    return response.json();
-}).then(function (json) {
-    // 2
-    console.log(JSON.stringify(json));
-
-    // 3
-    workData(json);
-
-}).catch(function(error) {
-    console.log(error);
-});
-
-// 3
-function workData(data) {
-    const workData1 = data.work1;
-    const workData2 = data.work2;
-
-    // console.log(data.work2)
-    // for (var i = 0; i < data.length; i++){
-    //     console.log(i)
-    // }
-    for (var i = 0; i < workData1.length; i++) {
-        console.log(data[i])
-        const title = workData1[i].title;
-        const subTitle = workData1[i].subTitle;
-        const categroy = workData1[i].categroy;
-        const part = workData1[i].part;
-        const date = workData1[i].date;
-        const device = workData1[i].device;
-        const lang = workData1[i].lang;
-        const percent = workData1[i].percent;
-        const url = workData1[i].url;
-        const review = workData1[i].review;
-
-
-
-        var sectionElement = document.createElement('section');
-        var h2Element = document.createElement('h2');
-        var elementGroup = document.createElement('div');
-        var p1Element = document.createElement('p');
-        var p2Element = document.createElement('span');
-
-        h2Element.textContent = workData1[i].title;
-        // for(const key of )
-        // for (const partItem of part){
-        //     const spanPartElement = document.createElement("span");
-        //     spanPartElement.textContent=partItem;
-        //     divElement.appendChild(spanPartElement);
+    ).then(function(response) {
+        return response.json();
+    }).then(function (json) {
+        // 2
+        console.log(JSON.stringify(json));
         
-        // }
-        // myPara1.textContent = 'Secret identity: ' + workData1[i].secretIdentity;
-        // myPara2.textContent = 'Age: ' + workData1[i].age;
-        // myPara3.textContent = 'Superpowers:';
-
-        // var superPowers = workData1[i].powers;
-        // for (var j = 0; j < superPowers.length; j++) {
-        //     var listItem = document.createElement('li');
-        //     listItem.textContent = superPowers[j];
-        //     myList.appendChild(listItem);
-        // }
-
-        sectionElement.appendChild(h2Element);
-        // myArticle.appendChild(myPara1);
-        // myArticle.appendChild(myPara2);
-        // myArticle.appendChild(myPara3);
-        // myArticle.appendChild(myList);
-
+        // 3
+        workData(json);
+        
+    }).catch(function(error) {
+        console.log(error);
+});
+    
+var sectionGroup = document.querySelector('#work');
+    
+function workData(data) {
+    for (const key in data) {
+        console.log(key);
+        console.log(key);
+        const sectionElement = document.createElement('section');
+        sectionElement.id = key;
+        sectionElement.classList.add('work');
+        const workItems = data[key];
         sectionGroup.appendChild(sectionElement);
+
+        for (let i = 0; i < workItems.length; i++) {
+   
+            const workItemSection = document.createElement('section');
+            workItemSection.id = `workitem${i + 1}`;
+            workItemSection.classList.add('workitem');
+
+            // category
+            const categoryElement = document.createElement('p');
+            categoryElement.classList.add(`work__${key}`);
+            categoryElement.textContent = `${key}`;
+
+            //title
+            const titleElement = document.createElement('h2');
+            titleElement.classList.add('title');
+            titleElement.textContent = workItems[i].title;
+            
+            //subtitle
+            const subtitleElement = document.createElement('h3');
+            subtitleElement.classList.add('subtitle');
+            subtitleElement.textContent = workItems[i].subTitle;
+            
+            //lang
+            const langElement = document.createElement('h4');
+            const langItem = workItems[i].lang;
+            langElement.classList.add('lang');
+
+            langItem.forEach((element) => {
+                const langItemElement = document.createElement('h5');
+                langItemElement.textContent = element;
+                langElement.appendChild(langItemElement);
+            });
+
+            //date
+            const dataElement = document.createElement('h5');
+            dataElement.classList.add('date');
+            dataElement.textContent = workItems[i].date;
+
+            //url
+            const urlElement = document.createElement('h5');
+            const urlItem = workItems[i].url;
+            urlElement.classList.add('url');
+            
+            Object.entries(urlItem).forEach(([key,values]) => {
+                console.log(key)
+                const urlItemElement = document.createElement('span');
+                const urlItemLink = document.createElement('a');
+                urlItemLink.setAttribute('href', `${values}`);
+                urlItemElement.classList.add(`${key}`);
+                urlItemLink.textContent = key;
+                urlItemElement.appendChild(urlItemLink);
+                urlElement.appendChild(urlItemElement);
+            });
+
+            //percent
+            const percentElement = document.createElement('h5');
+            percentElement.classList.add('percent');
+            percentElement.textContent = workItems[i].percent;
+
+
+            workItemSection.appendChild(categoryElement);
+            workItemSection.appendChild(titleElement);
+            workItemSection.appendChild(subtitleElement);
+            workItemSection.appendChild(langElement);
+            workItemSection.appendChild(dataElement);
+            workItemSection.appendChild(percentElement);
+            workItemSection.appendChild(urlElement);
+        
+            // 다른 요소들도 sectionElement에 추가
+            
+            
+
+            sectionElement.appendChild(workItemSection);
+        }
     }
-}
+  }
