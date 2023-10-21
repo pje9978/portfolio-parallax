96ca269ad3cc85153c65544741f352ffce828df7
{eunnnnnnn} <pje9978@gmail.com>
2023-10-21 16:36:42 +0900

    서브페이지 이미지

--- js/res1.js
+++ js/res1.js
@@ -11,19 +11,44 @@ document.addEventListener('DOMContentLoaded', function() {
          const container = document.getElementById('data-container');
          const title = document.querySelector('.title');
          const subTitle = document.querySelector('.subTitle');
+         const mackbook = document.querySelector('.viewport');
+         const iphone = document.querySelector('.display-content');
+         const pageLink = document.querySelectorAll('.pageLink');
+         const pageImg = document.querySelectorAll('.pageImg');
 
+
+         if (mackbook) {
+            console.log(mackbook);
+         } 
 
          data.forEach(itemData => {
             const itemId = itemData.id;
             console.log(itemId)
             if (itemId === parseInt(inputDataId)) {
                
+               // title 
                title.textContent = itemData.title;
                subTitle.textContent = itemData.subTitle;
+
+               // macbook Img
+               if (mackbook) {
+                  mackbook.style.backgroundImage = `url(${itemData.img.desktop[0]})`;
+               } 
+               
+               //page Img
+               pageImg.forEach((a,i) => {
+                  const imgUrl = itemData.img.desktop;
+                  a.src = imgUrl[i];
+               })
             
-            
-            
-            
+               //link Img
+               pageLink.forEach((a,i) => {
+                  const linkUrl = Object.values(itemData.url);
+                  a.href = linkUrl[i];
+               })
+
+               //iphone Img
+               iphone.style.backgroundImage = `url(${itemData.img.mobile[0]})`;
             }
          });
       })
